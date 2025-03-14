f6eaa1d3f4ed193b5cc0f3517f6d0b743795fc95
EldeVries <edevries14242@gmail.com>
2025-03-14 14:53:38 -0400

    updated current vs preposed team scripts

--- current_vs_preposed_team_lists.R
+++ current_vs_preposed_team_lists.R
@@ -2,4 +2,21 @@
 
 library(googlesheets4)
 
-current_team <- read_sheet()
+current_team <- read_sheet("https://docs.google.com/spreadsheets/d/1DTNgciErPLjp0YK5Punkrm563YJOtpmwkRTjrdSURK8/edit?gid=1634029129#gid=1634029129", sheet = "'Current Team'")
+
+back_ups <- read_sheet("https://docs.google.com/spreadsheets/d/1DTNgciErPLjp0YK5Punkrm563YJOtpmwkRTjrdSURK8/edit?gid=1634029129#gid=1634029129", sheet = "'Potential Team'")
+
+defenders <- ()
+  
+mids <- 
+  
+forwards <-
+  
+rucks <- 
+  
+
+comparison <- function(input, output) {
+  
+}
+
+                           
